0b9fc827a22877e832ab2d7729524e50118a4ecc
Felix <f@loetkolben>
2014-10-12 18:43:13 +0200

    repeat

--- pub/script.js
+++ pub/script.js
@@ -1,6 +1,7 @@
 btn = document.getElementById("snapshot");
 hider = document.getElementById("hider");
 wait = document.getElementById("wait");
+interval = document.getElementById("interval");
 count = 0;
 btn.onclick = function(e){
   console.log(this);
@@ -26,5 +27,11 @@ imgtag.onload = function(e){
   console.log(this);
   hider.style.display = "";
   wait.style.display = "none";
+
+  nextAction = parseInt(interval.value);
+  if(isNaN(nextAction))
+    return;
+  
+  setTimeout(btn.click(), nextAction*1000);
 }
 wait.style.display = "none";
